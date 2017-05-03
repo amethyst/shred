7d6c5907aa0db897653979761a78e9f4cf554857
torkleyy <thomas-schaller2000@gmx.de>
2017-05-03 18:00:06 +0200

    Add ThreadPool and task trait object
    ThreadPool
    + Dispatcher
    + DispatchBuilder
    + DispatchBuilder::with_pool
    
    task trait object
    + TaskDispatch
    + ExecTask
    + impl ExecTask for TaskDispatch

--- src/dispatch.rs
+++ src/dispatch.rs
@@ -1,10 +1,10 @@
 use std::sync::Arc;
 
 use fnv::FnvHashMap;
-use rayon::Scope;
+use rayon::{Configuration, Scope, ThreadPool};
 
 use bitset::AtomicBitSet;
-use {ResourceId, Task, TaskData};
+use {ResourceId, Resources, Task, TaskData};
 
 #[derive(Default)]
 pub struct Dependencies {
@@ -41,29 +41,35 @@ impl Dependencies {
     }
 }
 
-pub struct Dispatcher {
+pub struct Dispatcher<'r, 't> {
     dependencies: Dependencies,
     fulfilled: Vec<usize>,
     running: Arc<AtomicBitSet>,
-    tasks: Vec<TaskInfo>,
+    tasks: Vec<TaskInfo<'r, 't>>,
+    thread_pool: Arc<ThreadPool>,
+}
+
+impl<'r, 't> Dispatcher<'r, 't> {
+    pub fn dispatch(&mut self, res: &'r mut Resources) {}
 }
 
 #[derive(Default)]
-pub struct DispatcherBuilder {
+pub struct DispatcherBuilder<'r, 't> {
     dependencies: Dependencies,
     fulfilled: Vec<usize>,
     map: FnvHashMap<String, usize>,
-    tasks: Vec<TaskInfo>,
+    tasks: Vec<TaskInfo<'r, 't>>,
+    thread_pool: Option<Arc<ThreadPool>>,
 }
 
-impl DispatcherBuilder {
+impl<'r, 't> DispatcherBuilder<'r, 't> {
     pub fn new() -> Self {
         DispatcherBuilder::default()
     }
 
-    pub fn add<'a, T>(mut self, task: T, name: &str, dep: &[&str]) -> Self
-        where T: Task,
-              T::TaskData: TaskData<'a>
+    pub fn add<T>(mut self, mut task: T, name: &str, dep: &[&str]) -> Self
+        where T: Task + 't,
+              T::TaskData: TaskData<'r>
     {
         let id = self.tasks.len();
         let reads = unsafe { T::TaskData::reads() };
@@ -89,15 +95,22 @@ impl DispatcherBuilder {
             self.fulfilled.push(id);
         }
 
-        self.tasks.push(TaskInfo {
-            closure: Box::new(|| { unimplemented!() }),
+        let info = TaskInfo {
             dependents: Vec::new(),
-        });
+            exec: Box::new(TaskDispatch::new(id, task)) as Box<ExecTask>,
+        };
+        self.tasks.push(info);
 
         self
     }
 
-    pub fn finish(self) -> Dispatcher {
+    pub fn with_pool(mut self, pool: Arc<ThreadPool>) -> Self {
+        self.thread_pool = Some(pool);
+
+        self
+    }
+
+    pub fn finish(self) -> Dispatcher<'r, 't> {
         let size = self.tasks.len();
 
         Dispatcher {
@@ -105,11 +118,56 @@ impl DispatcherBuilder {
             fulfilled: self.fulfilled,
             running: Arc::new(AtomicBitSet::with_size(size)),
             tasks: self.tasks,
+            thread_pool: self.thread_pool
+                .unwrap_or_else(|| Self::create_thread_pool()),
         }
+    }
+
+    fn create_thread_pool() -> Arc<ThreadPool> {
+        Arc::new(ThreadPool::new(
+            Configuration::new()
+                .panic_handler(|x| println!("Panic in worker thread: {:?}", x)))
+            .expect("Invalid thread pool configuration"))
     }
 }
 
-struct TaskInfo {
-    closure: Box<FnMut()>,
+trait ExecTask<'r> {
+    fn exec<'b, 's, 'a>(&'b mut self, &'s Scope<'s>, &'r Resources, &'a AtomicBitSet)
+        where 'a: 's,
+              'b: 's;
+}
+
+struct TaskDispatch<T> {
+    id: usize,
+    task: T,
+}
+
+impl<T> TaskDispatch<T> {
+    fn new(id: usize, task: T) -> Self {
+        TaskDispatch { id: id, task: task }
+    }
+}
+
+impl<'r, T> ExecTask<'r> for TaskDispatch<T>
+    where T: Task,
+          T::TaskData: TaskData<'r>
+{
+    fn exec<'b, 's, 'a>(&'b mut self,
+                        scope: &'s Scope<'s>,
+                        res: &'r Resources,
+                        running: &'a AtomicBitSet)
+        where 'a: 's,
+              'b: 's
+    {
+        let data = T::TaskData::fetch(res);
+        scope.spawn(move |_| {
+                        self.task.work(data);
+                        running.set(self.id, false)
+                    })
+    }
+}
+
+struct TaskInfo<'r, 't> {
     dependents: Vec<usize>,
+    exec: Box<ExecTask<'r> + 't>,
 }
