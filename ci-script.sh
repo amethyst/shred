045751edafa96588f43b6b6e84508bc51f45848d
Azriel Hoh <azriel91@gmail.com>
2019-11-29 16:51:10 +1300

    Remove superfluous semicolons.

--- ci-script.sh
+++ ci-script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cargo build --verbose --no-default-features --features "shred-derive";
-cargo test --verbose --no-default-features --features "shred-derive";
-cargo build --verbose;
-cargo test --verbose;
+cargo build --verbose --no-default-features --features "shred-derive"
+cargo test --verbose --no-default-features --features "shred-derive"
+cargo build --verbose
+cargo test --verbose
